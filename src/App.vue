e3ff8d5a74ba1e36f89a68c305257294d3c12d20
Chisthoper Mella <chisthoper96@gmail.com>
2021-10-18 20:20:29 -0300

    contador vue

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <counter></counter>
+  </div>
+</template>
+
+<script>
+import Counter from '@/components/Counter'
+export default {
+  name: 'App',
+  components: {
+    Counter
+  }
+}
+</script>
+
+<style lang="stylus">
+#app
+  font-family Avenir, Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color #2c3e50
+  margin-top 60px
+</style>
